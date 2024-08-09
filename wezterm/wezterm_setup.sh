fa0cbe36c3607f902f1523278701e3cc852fbe1c
David Urbano <david.urbano@uao.edu.co>
2024-08-09 14:37:01 -0500

    fix(wezterm): handle execution from pipe and local file to avoid self-copy errors

--- wezterm/wezterm_setup.sh
+++ wezterm/wezterm_setup.sh
@@ -6,6 +6,7 @@ CONFIG_FILE=".wezterm.lua"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" && pwd)"
 SRC_FILE="$SCRIPT_DIR/$CONFIG_FILE"
 DEST_FILE="${HOME}/${CONFIG_FILE}"
+TMP_FILE="$(mktemp)"
 
 # Helper function to print status messages
 print_status() {
@@ -16,7 +17,8 @@ ensure_src_file_exists() {
     if [[ ! -f "$SRC_FILE" ]]; then
         print_status "Source configuration file $SRC_FILE does not exist."
         print_status "Creating a default source configuration file at $SRC_FILE..."
-        touch "$SRC_FILE"
+        touch "$TMP_FILE"
+        SRC_FILE="$TMP_FILE"
     fi
 }
 
@@ -32,6 +34,14 @@ copy_config_file() {
 
     print_status "Wezterm configuration installed/updated successfully."
 }
+
+cleanup() {
+    if [[ -f "$TMP_FILE" ]]; then
+        rm -f "$TMP_FILE"
+    fi
+}
+
+trap cleanup EXIT
 
 main() {
     copy_config_file
